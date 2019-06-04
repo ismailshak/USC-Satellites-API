de93d5d25d97b43c0face4de80869f7d790a1397
Ismail Elshakankiry <ismailshak94@yahoo.com>
2019-06-04 16:05:27 -0400

    parsed through source json file, created reduced json files. added model and erd diagrams

---
+++ planning/parser.js
@@ -0,0 +1,73 @@
+const data = require('../src/sourceData.json');
+const fs = require('fs');
+const path = require('path')
+
+// Parse through each object in the data array and extract relevant properties for a satellite
+let satArray = data.map(object => {
+    let sat = {
+        name: object.name,
+        UNRegistry: object['UN-Registry'],
+        country: object.country,
+        operator: object.operator,
+        user: object.user,
+        purpose: object.purpose,
+        detailedPurpose: object['detailed-purpose'],
+        classOfOrbit: object['class-of-orbit'],
+        typeOfOrbit: object['type-of-object'],
+        longitudeOfGeo: object['longitude-of-geo'],
+        perigree: object.perigree,
+        apogee: object.perigree,
+        eccentricity: object.eccentricity,
+        inclination: object.inclination,
+        period: object.period,
+        launchMass: object['launch-mass'],
+        dryMass: object['dry-mass'],
+        power: object.power,
+        dateOfLaunch: object['date-of-launch'],
+        expectedLifetime: object['expected-lifetime'],
+        contractor: object.contractor,
+        countryOfContractor: object['country-of-contractor'],
+        launchSite: object['launch-site'],
+        launchVehicle: object['launch-vehicle'],
+        cosparNumber: object['cospar-number'],
+        noradNumber: object['norad-number'],
+        comments: object.comments,
+        sources: [object.source, object.source__1, object.source__2, object.source__3, object.source__4, object.source__5]
+    }
+    return sat
+})
+
+// Parse through and extract data relevant to the country
+let countryArray = data.map(object => {
+    let country = {
+        name: object.country,
+        satellites: null,
+        contractors: null,
+    }
+    return country;
+})
+
+// Parse through and extract data relevant to the user
+let contractorArray = data.map(object => {
+    let contractor = {
+        name: object.contractor,
+        country: object.country,
+        satellites: null
+    }
+    return contractor;
+})
+
+fs.writeFile('./data/satelliteData.json', JSON.stringify(satArray), (err) => {
+    if(err) throw err;
+    console.log("Satellite data updated")
+})
+
+fs.writeFile('./data/countryData.json', JSON.stringify(countryArray), (err) => {
+    if(err) throw err;
+    console.log("Country data updated")
+})
+
+fs.writeFile('./data/contractorData.json', JSON.stringify(contractorArray), (err) => {
+    if(err) throw err;
+    console.log("Contractor data updated")
+})
