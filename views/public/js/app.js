f314c543c7f4684e62bc54ea6ec0a32ec6cb908a
Ismail Elshakankiry <ismailshak94@yahoo.com>
2019-06-09 21:53:17 -0400

    ready for deployment

--- views/public/js/app.js
+++ views/public/js/app.js
@@ -17,29 +17,29 @@ form.addEventListener('submit', e => {
 })
 
 
-var colors = new Array(
-    [0,0,0],
-    [30,0,30],
-    [0,0,70],
-    [0, 0, 0],
-    [0,0,70],
-    [0,0,0]);
+//-----------------------------------------
+//
+// The following color gradient codes source: 
+// https://codepen.io/quasimondo/pen/lDdrF
+//
+//-----------------------------------------
+
+var colors = new Array([0,0,0], [30,0,30], [0,0,70], [0, 0, 0], [0,0,70], [0,0,0]);
   
-  var step = 0;
-  //color table indices for: 
-  // current color left
-  // next color left
-  // current color right
-  // next color right
-  var colorIndices = [0,1,2,3];
+var step = 0;
+
+//color table indices for: 
+//current color left, next color left
+//current color right, next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient()
+{
   
-  //transition speed
-  var gradientSpeed = 0.002;
-  
-  function updateGradient()
-  {
-    
-    if ( $===undefined ) return;
+  if ( $===undefined ) return;
     
   var c0_0 = colors[colorIndices[0]];
   var c0_1 = colors[colorIndices[1]];
@@ -57,23 +57,23 @@ var colors = new Array(
   var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
   var color2 = "rgb("+r2+","+g2+","+b2+")";
   
-   $('body').css({
-     background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+  $('body').css({
+    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
     
-    step += gradientSpeed;
-    if ( step >= 1 )
-    {
-      step %= 1;
-      colorIndices[0] = colorIndices[1];
-      colorIndices[2] = colorIndices[3];
-      
-      //pick two new target color indices
-      //do not pick the same as the current one
-      colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      
-    }
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+    
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    
   }
-  
+}
+
   setInterval(updateGradient,10);
